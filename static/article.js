15679258085a42d727e8b1f5179baa7b40337505
xuhan <m13034045221@163.com>
2020-02-19 16:05:03 +0800

    解决所有bug

--- static/article.js
+++ static/article.js
@@ -1,13 +1,11 @@
 var id = getUrlParams('id');
-console.log(id);
 $.ajax({
     type: 'get',
     url: 'http://localhost:8080/api/v1/index/article',
     data: {
-        id:id
+        id: id
     },
     success: function (response) {
-        console.log(response);
         var html = template('articleTpl', response.data);
         $('#articleBox').html(html);
         $('.article_con').html(response.data.content);
