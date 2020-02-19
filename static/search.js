f194a7b3041fa2a00dde9b91436e45f608c83ce8
syli <594813687@qq.com>
2020-02-19 17:07:32 +0800

    前，后台文章列表搜索优化（李思源）

--- static/search.js
+++ static/search.js
@@ -1,40 +1,41 @@
 // 获取关键词的函数
 var keys = decodeURI(getUrlParams('key')) == -1 ? '' : decodeURI(getUrlParams('key'));
+$('.search_txt').val(keys);
 
 $.ajax({
-  type: 'get',
-  url: 'http://localhost:8080/api/v1/index/search',
-  data: {
-    key: keys
-  },
-  success: function (response) {
-    var html = template('postsTpl', { response: response.data.data });
-    $('#postsBox').html(html);
-    $('#pageBox').twbsPagination({
-      totalPages: response.data.pages,
-      visiblePages: 7,
-      first: '首页',
-      last: '末页',
-      prev: '上一页',
-      next: '下一页',
-      onPageClick: function (event, page) {
-        changePage(page);
-      }
-    });
-  }
-})
-// 分页
+        type: 'get',
+        url: 'http://localhost:8080/api/v1/index/search',
+        data: {
+            key: keys
+        },
+        success: function(response) {
+            var html = template('postsTpl', { response: response.data.data });
+            $('#postsBox').html(html);
+            $('#pageBox').twbsPagination({
+                totalPages: response.data.pages,
+                visiblePages: 7,
+                first: '首页',
+                last: '末页',
+                prev: '上一页',
+                next: '下一页',
+                onPageClick: function(event, page) {
+                    changePage(page);
+                }
+            });
+        }
+    })
+    // 分页
 function changePage(page) {
-  $.ajax({
-    type: 'get',
-    url: 'http://localhost:8080/api/v1/index/search',
-    data: {
-      key: keys,
-      page: page
-    },
-    success: function (response) {
-      var html = template('postsTpl', { response: response.data.data });
-      $('#postsBox').html(html);
-    }
-  })
+    $.ajax({
+        type: 'get',
+        url: 'http://localhost:8080/api/v1/index/search',
+        data: {
+            key: keys,
+            page: page
+        },
+        success: function(response) {
+            var html = template('postsTpl', { response: response.data.data });
+            $('#postsBox').html(html);
+        }
+    })
 }
