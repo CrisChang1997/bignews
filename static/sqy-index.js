bab3f25aa24d9c72e8aff03fa212721ecabb9d00
syli <594813687@qq.com>
2020-02-20 12:00:42 +0800

    最新评论修改

--- static/sqy-index.js
+++ static/sqy-index.js
@@ -2,12 +2,12 @@
 $.ajax({
     type: 'get',
     url: 'http://localhost:8080/api/v1/index/latest_comment',
-    success: function (re) {
+    success: function(re) {
         var lastCommentTpl = `
         {{each data}}
         <li>
         <span>{{$value.author.charAt(0)}}</span>
-        <b><em>{{$value.author}}</em onunload="Time({{$value.date}})"> {{$imports.Time($value.date)}}个月前(08-14)说:</b>
+        <b><em>{{$value.author}}</em> {{$imports.Time($value.date+' '+$value.time)}}说:</b>
         <strong>{{$value.intro}}</strong>
       </li>
       {{/each}}
@@ -19,12 +19,17 @@ $.ajax({
 });
 
 
-//时间
-function Time(time) {
-    var now = +new Date();
-    var inputTime = +new Date(time);
-    var times = (inputTime - now) / 1000;
-    var m = parseInt(times / 60 / 60 / 24 / 30);
-    return m;
+function Time(timestamp) {
+    timestamp = new Date(timestamp);
+    timestamp = timestamp.getTime();
 
+    var mistiming = Math.round((Date.now() - timestamp) / 1000);
+    var arrr = ['年', '个月', '星期', '天', '小时', '分钟', '秒'];
+    var arrn = [31536000, 2592000, 604800, 86400, 3600, 60, 1];
+    for (var i = 0; i < arrn.length; i++) {
+        var inm = Math.floor(mistiming / arrn[i]);
+        if (inm != 0) {
+            return inm + arrr[i] + '前';
+        }
+    }
 }
